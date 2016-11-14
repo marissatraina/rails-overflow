ac78077025d5b29d70f98ac9cd59c18a65106704
Marissa Traina <marissatraina@gmail.com>
2016-11-14 17:59:53 -0500

    final commit for the day

--- app/controllers/sessions_controller.rb
+++
@@ -1,5 +0,0 @@
-class SessionsController < ApplicationController
-  def new
-    @user = User.new(params[:user])
-  end
-end
--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -2,4 +2,14 @@ class UsersController < ApplicationController
   def new
     @user = User.new
   end
+
+  def login
+    @users = User.find_by(username: params[:username])
+    if @user && @user.authenticate(params[:password])
+      session[:user_id] = @user.id
+      redirect_to root
+    else
+      @errors = ["Your username and/or password were not found"]
+    end
+  end
 end
